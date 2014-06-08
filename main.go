ba2a32cb78ff5651881121525e710e705b754c3b
Reilly Watson <rwatson@kobo.com>
2014-06-08 18:06:11 -0400

    First commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"github.com/go-martini/martini"
+)
+
+type BookInfo struct {
+	Id string `json::id`
+	VolumeInfo struct {
+		Title string `json::Title`
+		Subtitle string `json::subtitle`
+		Authors []string `json::authors`
+		Description string `json::description`
+		ImageLinks struct {
+			SmallThumbnail string `json::smallThumbnail`
+			Thumbnail string `json::thumbnail`
+		} `json::imageLinks`
+		CanonicalVolumeLink string `json::canonicalVolumeLink`
+	} `json::volumeInfo`
+}
+
+type SearchResponse struct {
+	Items []BookInfo `json::items`
+}
+
+func search(req *http.Request) string {
+	apiUrl, _ := url.Parse("https://www.googleapis.com/books/v1/volumes")
+	params := url.Values{}
+	params.Set("q", req.URL.Query().Get("q"))
+	apiUrl.RawQuery = params.Encode()
+	println(apiUrl.String())
+	resp, err := http.Get(apiUrl.String())
+	if err != nil {
+		return "google failed!"
+	}
+	var m SearchResponse
+	body, _ := ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(body, &m)
+	println(len(m.Items))
+	if err != nil {
+		panic("whaaaaaaaaa!")
+	}
+	for _, element := range m.Items {
+		title := element.VolumeInfo.Title
+		authors := strings.Join(element.VolumeInfo.Authors, ", ")
+		println(authors)
+		println(title)
+	}
+	return "go"
+}
+
+func main() {
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello world!"
+	})
+	m.Get("/search", search)
+	m.Run()
+}
